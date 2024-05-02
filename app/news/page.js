4c1c9ce1635ede436adda873980d66d467c9ae5e
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-05-02 11:51:25 +0300

    Refactor news page to use NewsList component

--- app/news/page.js
+++ app/news/page.js
@@ -1,26 +1,11 @@
-import Link from "next/link";
-import React from "react";
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 const NewsPage = () => {
   return (
     <main>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <img
-                src={`/images/news/${newsItem.image}`}
-                alt={newsItem.title}
-                width="30"
-                height="30"
-              />
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </main>
   );
 };
