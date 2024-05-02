48a86b42bb55b88b88cc87167af9ec504d2946e1
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-05-02 11:50:39 +0300

    Refactor layout.js and add new news pages

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import MainHeader from "@/components/main-header/main-header";
+import MainHeader from "@/components/main-header";
 import "./globals.css";
 
 export const metadata = {
@@ -18,19 +17,4 @@ export default function RootLayout({ children }) {
       </body>
     </html>
   );
-=======
-import './globals.css';
-
-export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
-}
-
-export default function RootLayout({ children }) {
- return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
->>>>>>> 3040a3bf16eb6454247ed2ad86ad7d40945fbec8
 }
---
+++ app/news/[newSlug]/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewsNotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, we could not find the requested article.</p>
+    </div>
+  );
+};
+
+export default NewsNotFoundPage;
