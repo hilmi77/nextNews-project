7d8c9397ae3030b65d6a0da8912fc021dc75f7a2
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-05-08 11:44:48 +0300

    Refactor MainHeader to use NavLink component for navigation

--- components/main-header.js
+++ components/main-header.js
@@ -1,5 +1,6 @@
-import Link from "next/link";
 import React from "react";
+import NavLink from "./nav-link";
+import Link from "next/link";
 
 const MainHeader = () => {
   return (
@@ -10,7 +11,10 @@ const MainHeader = () => {
       <nav>
         <ul>
           <li>
-            <Link href="/news">News</Link>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink href="/archive">Archive</NavLink>
           </li>
         </ul>
       </nav>
