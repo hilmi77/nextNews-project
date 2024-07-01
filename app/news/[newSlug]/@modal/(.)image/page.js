ac260d169e9d80e0acf5022f946c5c02c9ff5483
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-01 14:16:58 +0300

    refactor: Improve error handling in InterceptedImagePage and useRouterhook

--- app/news/[newSlug]/@modal/(.)image/page.js
+++ app/news/[newSlug]/@modal/(.)image/page.js
@@ -1,15 +1,23 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound, useRouter } from "next/navigation";
 import React from "react";
 
 const InterceptedImagePage = ({ params }) => {
+  const router = useRouter();
   const newsItemSlug = params.newSlug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
   );
 
+  if (!newsItem) {
+    return notFound();
+  }
+
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img
