602dbf12d4b787702cd20a118f41cf111568f77f
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-02 14:18:45 +0300

    refactor: Fetch news data from API in NewsPage

---
+++ app/news/loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewsLoadingPage = () => {
+  return <div>NewsLoadingPage</div>;
+};
+
+export default NewsLoadingPage;
--- app/news/page.js
+++ app/news/page.js
@@ -1,20 +1,33 @@
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
 const NewsPage = async () => {
-  const response = await fetch("http://localhost:8080/news");
+  // const response = await fetch("http://localhost:8080/news");
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch news");
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch news");
+  // }
+
+  // const news = await response.json();
+
+  try {
+    const news = await getAllNews(); // Asenkron çağrı ve await kullanımı
+
+    return (
+      <main>
+        <h1>News Page</h1>
+        <NewsList news={news} />
+      </main>
+    );
+  } catch (error) {
+    console.error("Failed to fetch news", error);
+    return (
+      <main>
+        <h1>News Page</h1>
+        <p>Failed to load news.</p>
+      </main>
+    );
   }
-
-  const news = await response.json();
-
-  return (
-    <main>
-      <h1>News Page</h1>
-      <NewsList news={news} />
-    </main>
-  );
 };
 
 export default NewsPage;
