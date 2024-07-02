513fe7f2b49d99db5fdafe9310d86fe4fee1891e
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-02 13:05:04 +0300

    refactor: Fetch news data from API in NewsPage

--- app/news/page.js
+++ app/news/page.js
@@ -1,11 +1,18 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-const NewsPage = () => {
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
+  }
+
+  const news = await response.json();
+
   return (
     <main>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </main>
   );
 };
