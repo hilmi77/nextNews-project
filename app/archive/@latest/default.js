909dac37729e273f4b570712e302e39f88570871
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-03 12:54:29 +0300

    refactor: Optimize news data fetching and add lazy loading for images

--- app/archive/@latest/default.js
+++ app/archive/@latest/default.js
@@ -2,8 +2,8 @@ import NewsList from "@/components/news-list";
 import { getLatestNews } from "@/lib/news";
 import React from "react";
 
-const LatestNewsPage = () => {
-  const latestNews = getLatestNews();
+const LatestNewsPage = async () => {
+  const latestNews = await getLatestNews();
   return (
     <main>
       <h1>Latest News Page</h1>
---
+++ app/archive/loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArchiveLoadingPage = () => {
+  return <div>Archive Page Loading...</div>;
+};
+
+export default ArchiveLoadingPage;
